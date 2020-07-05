6c474a90d2e7807275472c898f4f8a3388decc29
Abid Zaidi <abidzaidi7@hotmail.com>
2020-07-06 01:22:29 +0500

    Adding Schema in sqlite

---
+++ src/sdk/database/scripts.sql
@@ -0,0 +1,34 @@
+
+CREATE TABLE "User"(
+  "Id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "Username" TEXT NOT NULL,
+  "Age" INTEGER NOT NULL,
+  "Email" TEXT NOT NULL,
+  "Gender" TEXT NOT NULL,
+  "DOB" TEXT NOT NULL
+);
+
+CREATE TABLE "Blog"(
+  "Id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "Title" TEXT NOT NULL,
+  "Content" TEXT,
+  "HeaderImagePath" TEXT,
+  "CreatedOn" TEXT,
+  "PublishedOn" TEXT,
+  "UpdatedOn" TEXT,
+  "Status" TEXT,
+  "UserId" INTEGER,
+  "Stargazers" INTEGER,
+  FOREIGN KEY("UserId") REFERENCES "User"("Id")
+);
+
+CREATE TABLE "Comment"(
+  "Id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  "BlogId" INTEGER NOT NULL,
+  "Comment" TEXT NOT NULL,
+  "UserId" INTEGER,
+  "addTEXT" TEXT,
+  "editTEXT" TEXT,
+  FOREIGN KEY("BlogId") REFERENCES "Blog"("BlogId"),
+  FOREIGN KEY("UserId") REFERENCES "User"("Id")
+);
